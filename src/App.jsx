47e83585bdc5d8de301ad9d8a4ef8abdc6fbfaad
Satyajit <sahoosatyajit2801@gmail.com>
2024-03-24 18:59:34 +0530

    integrate sentry

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import * as Sentry from "@sentry/react";
 
 const App = () => {
+
   return (
     <main className="bg-black">
       {/* Navbar */}
@@ -18,4 +20,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
