d36120c764fd3d76d05f6b710f10a959b47e48ad
Satyajit <sahoosatyajit2801@gmail.com>
2024-03-24 10:50:01 +0530

    added highlights secction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-// import Highlights from "./components/Highlights"
+import Highlights from "./components/Highlights"
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       {/* Hero */}
       <Hero />
       {/* Highlights */}
-      {/* <Highlights /> */}
+      <Highlights />
     </main>
   )
 }
